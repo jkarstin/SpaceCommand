b8ec54c50077e99eefb6629f062aed5a7051e919
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2020-04-22 07:36:59 -0700

    Added depth buffer bit to Core rendering

--- core-scg/src/ph/games/scg/Core.java
+++ core-scg/src/ph/games/scg/Core.java
@@ -47,7 +47,7 @@ public class Core extends Game {
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		if (this.screen != null) this.screen.render(Gdx.graphics.getDeltaTime());
 	}
 	
