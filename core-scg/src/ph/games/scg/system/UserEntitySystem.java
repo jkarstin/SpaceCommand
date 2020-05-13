30bfee97118ee47f56fdfee85067d27e25d55e75
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2020-05-12 19:27:39 -0700

    IT WORKED, now to refine

--- core-scg/src/ph/games/scg/system/UserEntitySystem.java
+++ core-scg/src/ph/games/scg/system/UserEntitySystem.java
@@ -37,6 +37,10 @@ public class UserEntitySystem extends EntitySystem implements EntityListener {
 			cc = entity.getComponent(CharacterComponent.class);
 			uec = entity.getComponent(UserEntityComponent.class);
 
+			//Get position
+			Vector3 position = new Vector3();
+			cc.ghostObject.getWorldTransform().getTranslation(position);
+			
 			//Get rotation
 			Quaternion quat = new Quaternion();
 			mc.instance.transform.getRotation(quat);
@@ -78,14 +82,11 @@ public class UserEntitySystem extends EntitySystem implements EntityListener {
 				}
 			}
 			
-			//Get current location
-			Vector3 position = new Vector3();
-			mc.instance.transform.getTranslation(position);
 			//Apply translation
 			position.add(translationVector);
 			mc.instance.transform.setTranslation(position);
 			//Apply rotation
-			mc.instance.transform.setToRotation(Vector3.Y, facing);
+			//mc.instance.transform.setToRotation(Vector3.Y, facing);
 			
 			cc.walkDirection.set(0f, 0f, 0f);
 			cc.walkDirection.add(translationVector);
